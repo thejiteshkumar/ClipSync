7e80f74c95f20926aa7ecb124be28d785e78e774
Jitesh Kumar <anjit193023@gmail.com>
2023-10-23 23:26:28 +0530

    Configuring System.Windows.Forms.Clipboard namespace in csproj

---
+++ ClipSync.Logic/ClipBoardManger.cs
@@ -0,0 +1,18 @@
+﻿namespace ClipSync.Logic
+{
+    public class ClipBoardManger : IClipboardManager
+    {
+
+        public Task GetClipBoardDetail()
+        {
+            var temp = Clipboard.GetText();
+
+            throw new NotImplementedException();
+        }
+
+        public Task SetClipBoardDetail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ClipSync.Logic/IClipboardManager.cs
+++ ClipSync.Logic/IClipboardManager.cs
@@ -2,5 +2,7 @@
 {
     public interface IClipboardManager
     {
+        Task GetClipBoardDetail();
+        Task SetClipBoardDetail();
     }
 }
