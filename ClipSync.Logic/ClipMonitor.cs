26b176d943263c484d5e9a22a2807d8ffb54bc3a
Jitesh Kumar <anjit193023@gmail.com>
2023-10-25 00:50:57 +0530

    Adding monitor

---
+++ ClipSync.Logic/ClipMonitor.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ClipSync.Logic
+{
+    public class ClipMonitor
+    {
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr viewer);
+
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+    }
+}
