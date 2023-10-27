2c593013dbef443e59457962302f54b198ff1d93
Jitesh Kumar <anjit193023@gmail.com>
2023-10-28 00:55:40 +0530

    Using the clipboard watcher into console app

---
+++ ClipSync.Console/CallClipboard.cs
@@ -0,0 +1,18 @@
+﻿using ClipSync.Logic;
+
+namespace ClipSync.Console
+{
+    public class CallClipboard
+    {
+        public void CallClipboardMonitor()
+        {
+            ClipMonitor.OnClipboardChange += ClipboardMonitor_OnClipboardChange;
+            ClipMonitor.Start();
+        }
+
+        private void ClipboardMonitor_OnClipboardChange(ClipboardFormat format, object data)
+        {
+            System.Console.WriteLine(data);
+        }
+    }
+}
--- ClipSync.Console/Program.cs
+++ ClipSync.Console/Program.cs
@@ -1,7 +1,13 @@
-﻿using ClipSync.Logic;
+﻿//Console.WriteLine("Hello, World!");
+//var temp = ClipBoardManger.GetText();
+//ClipBoardManger.SetText("This is test");
 
-Console.WriteLine("Hello, World!");
-var temp = ClipBoardManger.GetText();
-ClipBoardManger.SetText("This is test");
+using ClipSync.Console;
 
 Console.WriteLine("hello");
+
+CallClipboard callClipboard = new CallClipboard();
+
+callClipboard.CallMe();
+
+
