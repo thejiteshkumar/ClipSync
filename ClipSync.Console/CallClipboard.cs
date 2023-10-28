a8d70e15a6eab85128e2b2c2582fc275afaf1f36
Jitesh Kumar <anjit193023@gmail.com>
2023-10-28 15:23:11 +0530

    Writing the copied text to csv file

--- ClipSync.Console/CallClipboard.cs
+++ ClipSync.Console/CallClipboard.cs
@@ -1,4 +1,7 @@
 ﻿using ClipSync.Logic;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace ClipSync.Console
 {
@@ -13,6 +16,62 @@ namespace ClipSync.Console
         private void ClipboardMonitor_OnClipboardChange(ClipboardFormat format, object data)
         {
             System.Console.WriteLine(data);
+
+            if (data != null)
+            {
+                WriteToCSV(data.ToString());
+            }
+        }
+
+        //Use this to sync across different platforms
+        private void WriteToCSV(string copiedText)
+        {
+            string filePath = GetFilePath();
+
+            var records = GetRecordsForCSV(copiedText);
+
+            bool append = false;
+            bool addHeaders = true;
+            if (File.Exists(filePath))
+            {
+
+                append = true;
+                addHeaders = false;
+            }
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = addHeaders,
+                TrimOptions = TrimOptions.Trim,
+
+            };
+
+            using (var writer = new StreamWriter(filePath, append))
+            using (var csv = new CsvWriter(writer, config))
+            {
+                csv.WriteRecords(records);
+                writer.Flush();
+            }
+        }
+
+        private string GetFilePath()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ClipSync.csv");
+
+            //return Path.Combine("D:/CodeFiles/CSVTEST/ClipSync.csv");
+        }
+
+        private List<CsvModel> GetRecordsForCSV(string text)
+        {
+            return new List<CsvModel>()
+            {
+                new CsvModel
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    CopiedText = text,
+                    CreatedAt = DateTime.Now.ToString()
+                }
+            };
         }
     }
 }
---
+++ ClipSync.Console/CsvModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClipSync.Console
+{
+    public class CsvModel
+    {
+        public string Id { get; set; }
+        public string CopiedText { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
